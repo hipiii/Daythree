77b4fc5b644fbc20a747cef9e638c3173f576a21
hipii <durgamagar1098@gmail.com>
2024-12-06 11:41:20 +0545

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Navigation/Header'
+import { useLocation } from 'react-router-dom'
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const location = useLocation()
+  console.log(location,"location");
+  return (
+    <>
+      <Header></Header>
+      <Routes>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Navigation/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+    return (
+        <div className='bg-zinc-600 flex text-2xl h-2 border-b-2 p-8 gap-56 items-center  justify-between '>
+            <div className='bg-white'>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><g stroke="currentColor" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" clip-path="url(#siSettingsCuteLine0)"><path d="M23 12c0-1.66-1.34-3-3-3h-.76l.54-.54a3 3 0 0 0 0-4.24a3 3 0 0 0-4.24 0l-.54.54V4c0-1.66-1.34-3-3-3S9 2.34 9 4v.76l-.54-.54a3 3 0 0 0-4.24 0a3 3 0 0 0 0 4.24l.54.54H4c-1.66 0-3 1.34-3 3s1.34 3 3 3h.76l-.54.54a3 3 0 0 0 0 4.24a3 3 0 0 0 4.24 0l.54-.54V20c0 1.66 1.34 3 3 3s3-1.34 3-3v-.76l.54.54a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.24l-.54-.54H20c1.66 0 3-1.34 3-3Z"/><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/></g><defs><clipPath id="siSettingsCuteLine0"><path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></g></svg>
+            </div>
+            <Link to="/home">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to = "/contact">Contact</Link>
+        </div>
+    )
+}
+
+export default Header

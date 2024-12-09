7ec6c791a5d622db35d9d8cf495febace6e4ef34
hipii <durgamagar1098@gmail.com>
2024-12-09 16:47:47 +0545

    useState

--- src/Components/Navigation/Header.jsx
+++ src/Components/Navigation/Header.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import Model from '../UI/model'
 
 const Header = () => {
+    const [Loginshow, setLoginshow] = useState(false)
     return (
-        <div className='bg-zinc-600 flex text-2xl h-2 border-b-2 p-8 gap-56 items-center  justify-between '>
-            <div className='bg-white'>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><g stroke="currentColor" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" clip-path="url(#siSettingsCuteLine0)"><path d="M23 12c0-1.66-1.34-3-3-3h-.76l.54-.54a3 3 0 0 0 0-4.24a3 3 0 0 0-4.24 0l-.54.54V4c0-1.66-1.34-3-3-3S9 2.34 9 4v.76l-.54-.54a3 3 0 0 0-4.24 0a3 3 0 0 0 0 4.24l.54.54H4c-1.66 0-3 1.34-3 3s1.34 3 3 3h.76l-.54.54a3 3 0 0 0 0 4.24a3 3 0 0 0 4.24 0l.54-.54V20c0 1.66 1.34 3 3 3s3-1.34 3-3v-.76l.54.54a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.24l-.54-.54H20c1.66 0 3-1.34 3-3Z"/><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z"/></g><defs><clipPath id="siSettingsCuteLine0"><path fill="#fff" d="M0 0h24v24H0z"/></clipPath></defs></g></svg>
+        <div className='bg-zinc-600 flex text-2xl border-b-2 px-4 py-4 items-center  justify-between '>
+           {/* <Model Loginshow={Loginshow} setLoginshow={setLoginshow} >
+            <div className='w-96'></div>
+            <div>Login</div>
+            <div>
+                Durga
             </div>
-            <Link to="/home">Home</Link>
+           </Model> */}
+            <div className='bg-white'>
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none"><g stroke="currentColor" stroke-linejoin="round" stroke-miterlimit="10" stroke-width="1.5" clip-path="url(#siSettingsCuteLine0)"><path d="M23 12c0-1.66-1.34-3-3-3h-.76l.54-.54a3 3 0 0 0 0-4.24a3 3 0 0 0-4.24 0l-.54.54V4c0-1.66-1.34-3-3-3S9 2.34 9 4v.76l-.54-.54a3 3 0 0 0-4.24 0a3 3 0 0 0 0 4.24l.54.54H4c-1.66 0-3 1.34-3 3s1.34 3 3 3h.76l-.54.54a3 3 0 0 0 0 4.24a3 3 0 0 0 4.24 0l.54-.54V20c0 1.66 1.34 3 3 3s3-1.34 3-3v-.76l.54.54a3 3 0 0 0 4.24 0a3 3 0 0 0 0-4.24l-.54-.54H20c1.66 0 3-1.34 3-3Z" /><path d="M12 16a4 4 0 1 0 0-8a4 4 0 0 0 0 8Z" /></g><defs><clipPath id="siSettingsCuteLine0"><path fill="#fff" d="M0 0h24v24H0z" /></clipPath></defs></g></svg>
+            </div>
+            <Link to="/">Home</Link>
             <Link to="/about">About</Link>
-            <Link to = "/contact">Contact</Link>
+            <Link to="/contact">Contact</Link>
+            <div className='flex gap-20'>
+                <button onClick={() => {
+                    setLoginshow(true)
+                }}>Login</button>
+                <button>Registration</button>
+            </div>
         </div>
+        
+        
     )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Home = () => {
+  // const [abc,setAbc] = useState(0);
+  // function Add(){
+  //   setAbc(abc+1);
+  // }
+  // function Subtract(){
+  //   const result =(abc-1);
+  //   if(result<0){
+  //     alert("Sorry it is less than zero!!");
+  //   }else{
+  //     setAbc(abc-1);
+  //   }
+  // }
+    const[abc,setAbc] = useState(false);
+    function checkbox(){
+    setAbc(!abc);
+    }
+
+
+  
   return (
-    <div>
-        Home
+    <div className='flex gap-8 p-4'>
+        
+         <div className={`fixed ${abc?"translate-y-44":"-translate-y-96"} transition-all duration-1000 delay-75 ease-in-out top-0 left-0 bg-purple-400 w-fit h-fit p-9 right-0 mx-auto `}>
+          Durga
+        </div>
+        
+          <button onClick={()=>checkbox()} className={`${abc ? "bg-slate-500":"bg-purple-500"}`}>Clickme</button>
+        
+          {/* <button onClick={()=>Add()} className='bg-purple-600 h-12 w-16 rounded-md'>Add</button>   
+          <button onClick={()=>Subtract()} className='bg-pink-500 h-12 w-16 rounded-md'>Subtract</button>   */}
      </div>
   )
 }
